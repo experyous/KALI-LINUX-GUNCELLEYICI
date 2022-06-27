7983d66e8f5294d69ca47945c662987693ae4b33
experyous <71965927+experyous@users.noreply.github.com>
2022-06-27 17:05:57 +0300

    Update tst.sh

--- tst.sh
+++ tst.sh
@@ -3,43 +3,56 @@
 sudo apt update
 clear
 echo -e '%7 TAMAMLANDI ✅'
+sleep 3
 clear
 sudo apt upgrade
 clear
-echo -e '%14 TAMAMLANDI ✅'  
+echo -e '%14 TAMAMLANDI ✅'
+sleep 3
 apt list -upgradable
 clear
-echo -e '%21 TAMAMLANDI ✅'  
+echo -e '%21 TAMAMLANDI ✅'
+sleep 3
 sudo apt autoremove
 clear
-echo -e '%28 TAMAMLANDI ✅'  
+echo -e '%28 TAMAMLANDI ✅'
+sleep 3
 apt-get update
 clear
 echo -e '%35 TAMAMLANDI ✅'
+sleep 3
 apt-get upgrade
 clear
 echo -e '%42 TAMAMLANDI ✅'
+sleep 3
 pip install proxybroker
 clear
 echo -e '%49 TAMAMLANDI ✅'
+sleep 3
 apt-get install python
 clear
 echo -e '%56 TAMAMLANDI ✅'
+sleep 3
 apt update
 clear
 echo -e '%63 TAMAMLANDI ✅'
+sleep 3
 apt upgrade
 clear
 echo -e '%70 TAMAMLANDI ✅'
+sleep 3
 apt install python
 clear
 echo -e '%77 TAMAMLANDI ✅'
+sleep 3
 apt install git
 clear
 echo -e '%84 TAMAMLANDI ✅'
+sleep 3
 apt install dnsutils
 clear
 echo -e '%91 TAMAMLANDI ✅'
+sleep 3
 pip2 install -r requirements.txt
 clear
 echo -e '%100 TAMAMLANDI ✅'
@@ -51,5 +64,7 @@ echo -e 'CODER @experyous ✔️'
 sleep 6
 echo -e 'Telegram @experyous ✔️'
 sleep 6
+clear
+sudo su
 clear
 echo -e ':)'
